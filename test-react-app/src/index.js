30505a046e12788c6787cb04e2a97208cb3b14b0
Viaje9 <57859781+Viaje9@users.noreply.github.com>
2021-10-26 01:01:55 +0800

    新增function component

--- test-react-app/src/index.js
+++ test-react-app/src/index.js
@@ -1,36 +1,42 @@
-import { hot } from 'react-hot-loader/root';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { hot } from "react-hot-loader/root";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={function () {
-          console.log('ab');
-        }}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares });
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => {
+          this.handleClick(i);
+        }}
+      />
+    );
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = "Next player: X";
 
     return (
       <div>
@@ -73,6 +79,6 @@ class Game extends React.Component {
 
 // ========================================
 
-const App = ReactDOM.render(<Game />, document.getElementById('root'));
+const App = ReactDOM.render(<Game />, document.getElementById("root"));
 
 export default hot(App);
